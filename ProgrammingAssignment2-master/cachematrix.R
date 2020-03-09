1ca0e3318cd45a60bb5bdab5bbe1d62936edb301
PaulaBurkhardt <paso.burkhardt@gmail.com>
2020-03-09 21:05:23 +0100

    Finishing assignment

--- ProgrammingAssignment2-master/cachematrix.R
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -1,7 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-  
+
 ## Write a short comment describing this function
+#This function is there to invert a matrix, uses as a basis the example function given.
 
 makeCacheMatrix <- function(x = matrix()) {
 
